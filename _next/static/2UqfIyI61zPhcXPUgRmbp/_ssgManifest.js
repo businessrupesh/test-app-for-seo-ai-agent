64da41a541140a088a96ce750f69e456ecf1e9ea
rupeshmanda <rupesh.mandal@bizzbuzz.ai>
2026-06-01 07:55:19 +0545

    ccc

--- _next/static/2UqfIyI61zPhcXPUgRmbp/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[slug]","\u002Fblog\u002Fpage\u002F[num]","\u002Fproduct\u002F[slug]","\u002Fteam\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
